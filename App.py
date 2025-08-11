394d15e0e925f82bad54a368e4b5f24dab3c9ff5
sherman51 <92359221+sherman51@users.noreply.github.com>
2025-08-11 14:23:22 +0800

    Update App.py

--- App.py
+++ App.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import datetime
+import plotly.express as px
 
 
 st.title("Outbound Dashboard")
@@ -16,7 +17,6 @@ if uploaded_file is not None:
             df = pd.read_csv(uploaded_file, skiprows = 6)
         else:
             df = pd.read_excel(uploaded_file, skiprows = 6)
-
        
         # Show dataframe
         st.subheader("Preview of Data")
@@ -25,7 +25,34 @@ if uploaded_file is not None:
     except Exception as e:
         st.error(f"Error: {e}")
 
+
+    # Overview of Daily Orders
     
+    # Plotly Bar Chart
+    fig = px.bar(
+        df,
+        x='Category',
+        y='Sales',
+        text='Sales',
+        color='Category',
+        color_discrete_sequence=px.colors.sequential.Plasma_r,
+        title="Fruit Sales Overview",
+    )
+    
+    # Update layout for a modern look
+    fig.update_layout(
+        plot_bgcolor='white',
+        paper_bgcolor='white',
+        font=dict(size=14),
+        title_x=0.5,
+        margin=dict(l=40, r=40, t=60, b=40),
+    )
+    
+    # Add value labels
+    fig.update_traces(texttemplate='%{text}', textposition='outside')
+    
+    # Display chart in Streamlit
+    st.plotly_chart(fig, use_container_width=True)
 
 
 
