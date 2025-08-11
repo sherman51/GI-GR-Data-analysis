8115026b585804ba054139619172d93e7b9ad60f
sherman51 <92359221+sherman51@users.noreply.github.com>
2025-08-11 16:16:23 +0800

    Update App.py

--- App.py
+++ App.py
@@ -58,7 +58,7 @@ if uploaded_file:
         col_date, col_metric = st.columns([2, 1])
 
         with col_date:
-            st.metric(label="Date", selected_date)
+            st.metric(label="Date", selected_date.date)
 
         with col_metric:
             # Use GINo count if exists
@@ -169,6 +169,7 @@ if uploaded_file:
 
 else:
     st.info("Please upload an Excel file to view the dashboard.")
+
 
 
 
