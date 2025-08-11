24a8ef40ea9bca25bed66b9957038b0e77695353
sherman51 <92359221+sherman51@users.noreply.github.com>
2025-08-11 11:41:04 +0800

    Update App.py

--- App.py
+++ App.py
@@ -8,10 +8,11 @@ uploaded_file = st.sidebar.file_uploader("Choose an XLSX file", type=['xlsx'])
 if uploaded_file:
 
     df_raw = pd.read_excel(uploaded_file, skiprows=5)
+    st.table(df_raw)
 
-    print.df_raw()
 
     
+
 
 
 
