76714eaac772e92cf30ebb2b71d590e5f58163c4
sherman51 <92359221+sherman51@users.noreply.github.com>
2025-08-14 16:22:18 +0800

    Update App.py

--- App.py
+++ App.py
@@ -17,20 +17,21 @@ CONFIG = {
         '10-Open': 'Open',
         '15-Processing': 'Open',
         '20-Partially Allocated': 'Open',
-        '25-Fully Allocated': 'Pick-In Progress',
-        '35-Pick in Progress': 'Pick-In Progress',
+        '25-Fully Allocated': 'Pick In-Progress',
+        '35-Pick in Progress': 'Pick In-Progress',
         '45-Picked': 'Picked',
         '65-Packed': 'Packed',
         '75-Shipped': 'Shipped',
         '98-Cancelled': 'Cancelled'
     },
     "order_types": ['Back Orders', 'normal', 'Ad-hoc Normal', 'Ad-hoc Urgent', 'Ad-hoc Critical'],
-    "status_segments": ['Open','Pick-In Progress', 'Picked', 'Packed', 'Shipped', 'Cancelled'],
+    "status_segments": ['Open','Pick In-Progress', 'Picked', 'Packed', 'Shipped', 'Cancelled'],
     "colors": {
         'Shipped': 'green',
         'Cancelled': 'red',
         'Packed': 'blue',
         'Picked': 'yellow',
+        'Pick In-Progress': 'Orange',
         'Open': 'salmon',
         'Ad-hoc Urgent': 'orange',
         'Ad-hoc Critical': 'crimson'
@@ -424,6 +425,7 @@ if uploaded_file:
 
 else:
     st.warning("📄 Please upload an Excel file to begin.")
+
 
 
 
