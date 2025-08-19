e839e14ddcfe337e527268161416829337b3b791
sherman51 <92359221+sherman51@users.noreply.github.com>
2025-08-19 15:41:55 +0800

    Update App.py

--- App.py
+++ App.py
@@ -333,7 +333,7 @@ def performance_metrics(df, key_prefix=""):
 date_list = []
 days_checked = 0
 #current_date = datetime.today().date()
-current_date = date(2025,8,22)
+current_date = date(2025,8,15)
 while len(date_list) < 3 and days_checked < 7:
     weekday = current_date.weekday()  # Monday = 0, Sunday = 6
 
@@ -431,6 +431,7 @@ with col2:
     performance_metrics(df, key_prefix="overall")
 
 st.markdown("###  *Stay Safe & Well*")
+
 
 
 
